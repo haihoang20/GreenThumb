5df71f65a2a8d021d9c139145c27af3eb1290470
Hai Hoang <hai.manh.hoang@gmail.com>
2015-03-15 05:28:28 -0700

    added more images and layolayout stuff

--- app/src/main/java/beangames/greenthumb/Info.java
+++ app/src/main/java/beangames/greenthumb/Info.java
@@ -1,17 +1,33 @@
 package beangames.greenthumb;
 
+import android.graphics.Typeface;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class Info extends ActionBarActivity {
+
+    private TextView thanks;
+    private TextView topBar;
+    private TextView bottomBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+        String fontPath1  = "fonts/Alittlesunshine.ttf";
+        Typeface plantText = Typeface.createFromAsset(getAssets(),fontPath1);
+        thanks = (TextView)findViewById(R.id.thank_you);
+        thanks.setTypeface(plantText);
+        String fontPath = "fonts/Chocolate.ttf";
+        Typeface menu   = Typeface.createFromAsset(getAssets(),fontPath);
+        topBar = (TextView)findViewById(R.id.top_bar);
+        bottomBar = (TextView)findViewById(R.id.bottom_bar);
+        topBar.setTypeface(menu);
+        bottomBar.setTypeface(menu);
     }
 
 
