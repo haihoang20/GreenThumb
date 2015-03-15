62a23de2b3e9c3bb814d58b034e9e1667178b2fb
Hai Hoang <hai.manh.hoang@gmail.com>
2015-03-15 06:52:51 -0700

    Finished adding photos and finished all layout stuff

--- app/src/main/java/beangames/greenthumb/PlantActivity.java
+++ app/src/main/java/beangames/greenthumb/PlantActivity.java
@@ -122,6 +122,7 @@ public class PlantActivity extends MainActivity{
         depth.setTypeface(plantText);
         rowSpacing.setTypeface(plantText);
         plantSpacing.setTypeface(plantText);
+        name.setTypeface(plantText);
 
     }
     public void addPlantList(View v) {
